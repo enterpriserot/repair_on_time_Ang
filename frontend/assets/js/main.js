0749ef8827644cd3ae246dc3363417e8590520d5
Antonio Revert <a.revert.hinojosa@gmail.com>
2016-12-26 13:09:13 +0100

    Added missing files and changed file route in index

---
+++ frontend/assets/js/main.js
@@ -0,0 +1,25 @@
+function amigable1(url) {
+    var link="";
+    url = url.replace("?", "");
+    url = url.split("&");
+
+    for (var i=0;i<url.length;i++) {
+        var aux = url[i].split("=");
+        link +=  "/"+aux[1];
+    }
+    return link;
+}
+
+function amigable(url) {
+    var link="";
+    url = url.replace("?", "");
+    url = url.split("&");
+
+    for (var i=0;i<url.length;i++) {
+        var aux = url[i].split("=");
+        link +=  "/"+aux[1];
+    }
+    return "http://localhost/repair_on_time" + link;
+    //return "http://arevert.tk/repair_on_time" + link;
+    // return "https://projects-alumnes-yomogan.c9users.io/proj_final_login/JoinElderly" + link;
+}
